f12e7ad66bbb4bb040e67292b409d85aefe8242d
dranoer <dranoer@gmail.com>
2021-10-13 14:02:27 +0330

    add: Simple card

--- app/src/main/java/com/dranoer/uoweme/MainActivity.kt
+++ app/src/main/java/com/dranoer/uoweme/MainActivity.kt
@@ -3,10 +3,20 @@ package com.dranoer.uoweme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dranoer.uoweme.model.Expense
+import com.dranoer.uoweme.model.mockExpenses
 import com.dranoer.uoweme.ui.theme.UoweMETheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +26,8 @@ class MainActivity : ComponentActivity() {
             UoweMETheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ExpenseList()
+
+                    ExpenseList(mockExpenses)
                 }
             }
         }
@@ -24,14 +35,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ExpenseList() {
+fun ExpenseList(expenses: List<Expense>) {
+    Column() {
+        for (item in expenses) {
+            ExpenseCart(expense = item)
+        }
+    }
+}
 
+@Composable
+fun ExpenseCart(expense: Expense) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = Color(0xFF866EE1),
+    ) {
+        Text(text = expense.title, modifier = Modifier.padding(8.dp))
+    }
 }
 
 @Preview
 @Composable
 fun ExpenseListPreview() {
     UoweMETheme {
-        ExpenseList()
+        ExpenseList(mockExpenses)
     }
+}
+
+@Preview
+@Composable
+fun ExpenseCartPreview() {
+    ExpenseCart(expense = mockExpenses[0])
 }
